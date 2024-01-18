6fce9671d8b263e712ad8dbae30ef7acb44237aa
Anugrah <71122669+Anugrah070@users.noreply.github.com>
2024-01-18 19:04:45 +0530

    ag

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,8 +6,8 @@ export default function Navbar(Props){
         
         // <nav className="Main_navbar">
         <nav className='Main_navbar'>
-          <a href="/"><img src="assets/logo.png" alt="logo" /></a>
-          <img id="Logo" src="assets/menu.png" alt="menu" onClick={Props.clickHandle}/>
+          <a href="/"><img src="./assets/logo.png" alt="logo" /></a>
+          <img id="Logo" src="./assets/menu.png" alt="menu" onClick={Props.clickHandle}/>
         </nav>
   
       
